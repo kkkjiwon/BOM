4029efa47aa56570b0115a0c3b276d0a6347483b
soon <makt00byoung@gmail.com>
2024-04-23 10:06:43 +0900

    Revert "반응형수정"
    This reverts commit 6ead16b9245fa9a6f6f8081f1d6c6c38ab01810b.

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ window.onload = function () {
       hide: true,
     },
   });
-  // s_program
   var swiper = new Swiper(".sw-program", {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -39,43 +38,4 @@ window.onload = function () {
       },
     },
   });
-
-  // 글자 모션
-  var observer = new IntersectionObserver(
-    function(entries) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("active");
-        } else {
-          entry.target.classList.remove("active");
-        }
-      });
-    },
-    { threshold: 0.5 }
-  );
-
-  var mentorSection = document.querySelector(".s_Mentor_des");
-  observer.observe(mentorSection);
-
-  var letterSection = document.querySelector(".s_letter_right");
-  observer.observe(letterSection);
 };
-
-
-$(document).ready(function () {
-  $(".main-menu").mouseenter(function () {
-    $(".sub-menu li , .navbt").stop().slideDown();
-  });
-  $(".main-menu").mouseleave(function () {
-    $(".sub-menu li , .navbt").stop().slideUp();
-  });
-  $(".menu > a").click(function (event) {
-    event.preventDefault(); // 기본 이벤트 방지
-
-    let submenu = $(this).next(".hide");
-    submenu.slideToggle(); // 부드럽게 슬라이드 업 또는 다운
-
-    // 화살표 회전 토글
-    $(this).find('.material-symbols-outlined').toggleClass('active');
-  });
-});
