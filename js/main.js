48f4dc2f97cd5d5331b6baff69464e296a7f6f91
soon <makt00byoung@gmail.com>
2024-04-26 16:40:20 +0900

    반응형 레이아웃 수정

--- js/main.js
+++ js/main.js
@@ -13,6 +13,30 @@ window.onload = function () {
       header.classList.remove("active");
     }
   });
+  // 상단 스크롤 기능
+  const navMb = document.querySelector(".nav-mb");
+  const overlay = document.querySelector(".overlay");
+  
+  // 햄버거 버튼 클릭 이벤트
+  mbt.addEventListener("click", function () {
+    const state = this.classList.contains("ani");
+    if (state) {
+      this.classList.remove("ani");
+      navMb.classList.remove("active");
+      overlay.classList.remove("active");
+    } else {
+      this.classList.add("ani");
+      navMb.classList.add("active");
+      overlay.classList.add("active");
+    }
+  });
+
+  // 오버레이 클릭 시 메뉴 닫기
+  overlay.addEventListener("click", function () {
+    mbt.classList.remove("ani");
+    navMb.classList.remove("active");
+    overlay.classList.remove("active");
+  });
   // s_visual
   var swiper = new Swiper(".sw-vistual", {
     scrollbar: {
@@ -63,12 +87,12 @@ window.onload = function () {
 
 
 $(document).ready(function () {
-  $(".main-menu").mouseenter(function () {
-    $(".sub-menu li , .navbt").stop().slideDown();
-  });
-  $(".main-menu").mouseleave(function () {
-    $(".sub-menu li , .navbt").stop().slideUp();
-  });
+  // $(".main-menu").mouseenter(function () {
+  //   $(".sub-menu li , .navbt").stop().slideDown();
+  // });
+  // $(".main-menu").mouseleave(function () {
+  //   $(".sub-menu li , .navbt").stop().slideUp();
+  // });
   $(".menu > a").click(function (event) {
     event.preventDefault(); // 기본 이벤트 방지
 
